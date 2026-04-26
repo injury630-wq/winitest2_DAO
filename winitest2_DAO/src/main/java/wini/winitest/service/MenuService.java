84cd913644665b7e3e5a3febea8f0b88f93aa773
jeack9 <jeack99@g.yju.ac.kr>
2026-04-27 05:33:17 +0900

    dev

--- winitest2_DAO/src/main/java/wini/winitest/service/MenuService.java
+++
@@ -1,8 +0,0 @@
-package wini.winitest.service;
-
-import java.util.List;
-import java.util.Map;
-
-public interface MenuService {
-	List<Map<String, Object>> selectMenuList() throws Exception;
-}
--- winitest2_DAO/src/main/webapp/WEB-INF/views/user/userManageDummy.jsp
+++
@@ -1,2 +0,0 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java"%>
-<%-- 구버전 백업 파일 (미사용) - 현재 사용 파일: userManage.jsp --%>
